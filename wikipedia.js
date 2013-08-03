770d67af713544983a0f429c1a0490bf83d55b78
ancap <admin@hlcs.ru>
2013-08-03 10:47:40 +0400

    1

--- wikipedia.js
+++ wikipedia.js
@@ -1,5 +1,51 @@
 $(document).ready(function() {
 
-  $("h1").hide();
+$("pre").css({
+
+  "white-space": "pre-wrap"
+
+});
+
+$("a.internal > img").each(function() {
+
+  var src = $(this).attr("src");
+
+  if(src.indexOf("magnify-clip") === -1) return true;
+// атрибут target blank для иконки под картинкой
+  $(this).parent("a").attr("target", "_blank");
+//     $("body").prepend(src + "<br />");
+  return true;
+
+});
+
+$("a.image").each(function() {
+
+  $(this).attr({
+    target: "_blank"
+  });
+
+  return true;
+
+});
+
+function getHostname(url) {
+
+  var hostnameRegexp = new RegExp("^(http(s|):|)\/\/[^/]+");
+  var m = url.match(hostnameRegexp);
+  return m ? m[0].replace("http://", "").replace("https://", "").replace("www\.", "").replace("//", "") : null;
+
+}
+
+$("a").each(function() {
+
+  var linkHref = $(this).attr("href");
+  if (!linkHref) return true;
+  linkHref = getHostname(linkHref);
+
+  if (linkHref !== location.host && linkHref !== null) $(this).attr("target", "_blank");
+// $("body").prepend(linkHref + " " + location.host + "<br />");
+  return true;
+
+});
 
 });
